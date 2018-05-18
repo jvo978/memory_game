da9c5eafb7a05534a6b3dac97ff9e52b45a333a5
Jason Vo <jasonv.vo@gmail.com>
2018-05-17 23:53:14 -0400

    create game logic

--- js/main.js
+++ js/main.js
@@ -28,35 +28,42 @@ var cards = [
 
 var cardsInPlay = [];
 
-var checkForMatch = function(){
 
+var checkForMatch = function(){
+ 
+if (cardsInPlay.length === 2 || cardsInPlay.length === 4) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 	alert("You found a match!");
 }
  else {
  	alert("Sorry, try again.");
  }
-
+}
 }
 
 
-
-
-
-var flipCard = function(cardId) {
-console.log("User flipped " + cards[cardId].rank);
-console.log("User flipped " + cards[cardId].suit);
-console.log("User flipped " + cards[cardId].cardImage);
+var flipCard = function() {
+var cardId = this.getAttribute('data-id');
+this.setAttribute('src', cards[cardId].cardImage);
 cardsInPlay.push(cards[cardId].rank);
 checkForMatch();
-
 }
 
-flipCard(0);
-flipCard(2);
+
+
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++){
+	var cardElement = document.createElement('img');
+	cardElement.setAttribute('src', 'images/back.png');
+	cardElement.setAttribute('data-id', i);
+	cardElement.addEventListener('click', flipCard);
+	document.getElementById("game-board").appendChild(cardElement);	
+	}
+}
 
 
 
+	createBoard();
 
 
 
